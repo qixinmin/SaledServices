a29f03e48201b2ae2a61b895a2e0cf09acd2d330
qxm <qxmin1984@126.com>
2018-12-06 10:39:11 +0800

    fix bugs
    add report

--- SaledServices/Export/StoreHouseStatisticsExport.cs
+++ SaledServices/Export/StoreHouseStatisticsExport.cs
@@ -74,7 +74,7 @@ namespace SaledServices.Export
             string endTime = this.dateTimePickerend.Value.ToString("yyyy-MM-dd",System.Globalization.DateTimeFormatInfo.InvariantInfo);
 
             List<StoreHouseStatisticsStruct> receiveOrderList = new List<StoreHouseStatisticsStruct>();
-
+            Dictionary<string, bgaTypeStatistics> bgabriefdict = new Dictionary<string, bgaTypeStatistics>();
             try
             {
                 SqlConnection mConn = new SqlConnection(Constlist.ConStr);
@@ -161,18 +161,29 @@ namespace SaledServices.Export
                             }
                         case "BGA":
                             {
-                                cmd.CommandText = "select input_number from bga_in_stock where mpn ='" + stockcheck.mpn.Split('_')[0] + "' and input_date between '" + startTime + "' and '" + endTime + "'";
+                                //只是获取简称信息，不需要加入时间限制
+                                cmd.CommandText = "select bga_describe from bga_in_stock where mpn ='" + stockcheck.mpn.Split('_')[0] + "'";                              
+                                querySdr = cmd.ExecuteReader();
+                                while (querySdr.Read())
+                                {                                  
+                                    stockcheck.bga_brief = querySdr[0].ToString();
+                                    break;
+                                }
+                                querySdr.Close();
+
+                                cmd.CommandText = "select input_number,bga_describe from bga_in_stock where mpn ='" + stockcheck.mpn.Split('_')[0] + "' and input_date between '" + startTime + "' and '" + endTime + "'";
                                 int bgainnumber = 0;
                                 querySdr = cmd.ExecuteReader();
                                 while (querySdr.Read())
                                 {
                                     bgainnumber += Int32.Parse(querySdr[0].ToString());
+                                    stockcheck.bga_brief = querySdr[1].ToString();
                                    // break;
                                 }
                                 querySdr.Close();
                                 stockcheck.buynumber = bgainnumber + "";
 
-                                cmd.CommandText = "select out_number from bga_out_stock where mpn ='" + stockcheck.mpn.Split('_')[0] + "' and input_date between '" + startTime + "' and '" + endTime + "'";
+                                cmd.CommandText = "select out_number from bga_out_stock where mpn like '%" + stockcheck.mpn + "%' and input_date between '" + startTime + "' and '" + endTime + "'";
                                 int bgaoutnumber = 0;
                                 querySdr = cmd.ExecuteReader();
                                 while (querySdr.Read())
@@ -213,6 +224,45 @@ namespace SaledServices.Export
                     }
                 }
 
+                //按bga简称来统计信息
+              
+                
+                foreach (StoreHouseStatisticsStruct stockcheck in receiveOrderList)
+                {
+                    if (stockcheck.bga_brief != null)
+                    {
+                        if (bgabriefdict.Keys == null || bgabriefdict.Keys.Contains(stockcheck.bga_brief) == false)
+                        {
+                            // List<bgaTypeStatistics> bgabrieflist = new List<bgaTypeStatistics>();
+                            bgaTypeStatistics temp = new bgaTypeStatistics();
+                            temp.bga_brief = stockcheck.bga_brief;
+                            temp.buynumber = stockcheck.buynumber;
+                            temp.leftnumber = stockcheck.leftnumber;
+                            temp.outnumber = stockcheck.outnumber;
+                            temp.describe = stockcheck.describe;
+
+                            temp.releatedMpn = stockcheck.mpn;
+                            //bgabrieflist.Add(temp);
+                            bgabriefdict.Add(temp.bga_brief, temp);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                bgaTypeStatistics temp = bgabriefdict[stockcheck.bga_brief];
+                                temp.releatedMpn += "," + stockcheck.mpn;
+                                temp.buynumber = Int16.Parse(temp.buynumber) + Int16.Parse(stockcheck.buynumber) + "";
+                                temp.leftnumber = Int16.Parse(temp.leftnumber) + Int16.Parse(stockcheck.leftnumber) + "";
+                                temp.outnumber = Int16.Parse(temp.outnumber) + Int16.Parse(stockcheck.outnumber) + "";
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.ToString());
+                            }
+                        }
+                    }                   
+                }
+
                 mConn.Close();
             }
             catch (Exception ex)
@@ -220,24 +270,30 @@ namespace SaledServices.Export
                 MessageBox.Show(ex.ToString());
             }
 
-            generateExcelToCheck(receiveOrderList, startTime, endTime);
+            generateExcelToCheck(receiveOrderList,bgabriefdict, startTime, endTime);
         }
 
-        public void generateExcelToCheck(List<StoreHouseStatisticsStruct> StockCheckList, string startTime, string endTime)
+        public void generateExcelToCheck(List<StoreHouseStatisticsStruct> StockCheckList, Dictionary<string, bgaTypeStatistics> bgabriefdict,
+            string startTime, string endTime)
         {
-            List<string> titleList = new List<string>();
-            List<Object> contentList = new List<object>();
-                 
-            titleList.Add("ID");
-            titleList.Add("库房");
-            titleList.Add("储位");
-            titleList.Add("存储料号");
-            titleList.Add("类型");
-            titleList.Add("厂商");
-            titleList.Add("剩余数量");
-            titleList.Add("购买数量");
-            titleList.Add("出库数量");
-            titleList.Add("描述");
+            List<allContent> allcontentList = new List<allContent>();
+
+            allContent firstsheet = new allContent();
+            firstsheet.sheetName = this.houseComboBox.Text + "平衡表信息导出" + startTime.Replace('/', '-') + "-" + endTime.Replace('/', '-');
+            firstsheet.titleList = new List<string>();
+            firstsheet.contentList = new List<object>();
+
+            firstsheet.titleList.Add("ID");
+            firstsheet.titleList.Add("库房");
+            firstsheet.titleList.Add("储位");
+            firstsheet.titleList.Add("存储料号");
+            firstsheet.titleList.Add("类型");
+            firstsheet.titleList.Add("厂商");
+            firstsheet.titleList.Add("剩余数量");
+            firstsheet.titleList.Add("购买数量");
+            firstsheet.titleList.Add("出库数量");
+            firstsheet.titleList.Add("描述");
+            firstsheet.titleList.Add("BGA简称");
 
             foreach (StoreHouseStatisticsStruct stockcheck in StockCheckList)
             {
@@ -253,12 +309,44 @@ namespace SaledServices.Export
                 ct1.Add(stockcheck.buynumber);
                 ct1.Add(stockcheck.outnumber);
                 ct1.Add(stockcheck.describe);
+                ct1.Add(stockcheck.bga_brief);
 
                 ctest1.contentArray = ct1;
-                contentList.Add(ctest1);
+                firstsheet.contentList.Add(ctest1);
             }
+            allcontentList.Add(firstsheet);
 
-            Utils.createExcel("D:\\"+this.houseComboBox.Text+"平衡表信息导出"+ startTime.Replace('/', '-') + "-" + endTime.Replace('/', '-')  + ".xlsx", titleList, contentList);
+            allContent secondsheet = new allContent();
+            secondsheet.sheetName = "BGA简称" + startTime.Replace('/', '-') + "-" + endTime.Replace('/', '-');
+            secondsheet.titleList = new List<string>();
+            secondsheet.contentList = new List<object>();
+            
+            secondsheet.titleList.Add("BGA类型");
+            secondsheet.titleList.Add("剩余数量");
+            secondsheet.titleList.Add("购买数量");
+            secondsheet.titleList.Add("出货数量");
+            secondsheet.titleList.Add("描述");
+            secondsheet.titleList.Add("相关mpn");
+
+            foreach (string key in bgabriefdict.Keys)
+            {
+                ExportExcelContent ctest1 = new ExportExcelContent();
+
+                List<string> ct1 = new List<string>();
+                bgaTypeStatistics stockcheck = bgabriefdict[key];
+                ct1.Add(stockcheck.bga_brief);
+                ct1.Add(stockcheck.leftnumber);
+                ct1.Add(stockcheck.buynumber);
+                ct1.Add(stockcheck.outnumber);
+                ct1.Add(stockcheck.describe);
+                ct1.Add(stockcheck.releatedMpn);
+
+                ctest1.contentArray = ct1;
+                secondsheet.contentList.Add(ctest1);
+            } 
+            allcontentList.Add(secondsheet);
+
+            Utils.createMulitSheetsUsingNPOI(string.Format("D:\\{0}平衡表信息导出{1}-{2}", this.houseComboBox.Text, startTime.Replace('/', '-'), endTime.Replace('/', '-')) + ".xls", allcontentList);
         }
     }
 
@@ -276,5 +364,19 @@ namespace SaledServices.Export
 
         public string vendor;
         public string describe;
+
+        public string bga_brief;
+    }
+
+   public class bgaTypeStatistics
+   {
+       public string bga_brief;
+       public string leftnumber;
+
+       public string buynumber;
+       public string outnumber;
+       public string describe;
+
+       public string releatedMpn;//所有类型的汇总
     }
 }
